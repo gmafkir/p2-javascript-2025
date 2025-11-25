648ce999580389163b5f91435e2fae760e928b1e
gmafkir <9020340@student.zadkine.nl>
2025-11-25 10:21:46 +0100

    bonus toegevoegd

--- opdracht 1/main.js
+++ opdracht 1/main.js
@@ -1,2 +1,3 @@
 document.getElementById("titel").style.color = "purple";
 document.getElementById("insert").innerHTML = "Hallo";
+alert ("Een id is uniek, dus als het meerdere keren wordt gebruikt krijgt alles met hetzelfde id hetzelfde aanpassing.");
