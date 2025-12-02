4d4aa8d4ad3aca225fd5bdb2cd4b0051ca4aea1b
gmafkir <9020340@student.zadkine.nl>
2025-12-02 10:15:41 +0100

    opdracht 6 af

---
+++ opdracht 6/main.js
@@ -0,0 +1,6 @@
+function kleur_aanpassen() {
+    document.getElementById("veranderText").style.color = "purple";
+    document.getElementById("veranderText").innerText = "text aangepast!";
+}
+
+kleur_aanpassen();
